3d4bfa53adcb9538bcf6732e27ad53a902f85c92
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-05-11 00:18:59 +0900

    [level 3] Title: 양과 늑대, Time: 0.45 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,63 +1,41 @@
-def dfs(idx, sheep, wolf, possible):
-    global g_info, answer, graph
-    if g_info[idx] == 0:
-        sheep += 1
-        answer = max(answer, sheep)
-    else:
-        wolf += 1
-        
-    if wolf >= sheep:
-        return 
-    
-    possible.extend(graph[idx])
-    for p in possible:
-        dfs(p, sheep, wolf, [i for i in possible if i != p])
-        
-def solution(info, edges):
-    global answer, g_info, visited, graph
-    answer = 0
-    g_info = info
-    n = len(info)
-    graph = [[] for _ in range(n)]
-    
-    for a, b in edges:
-        graph[a].append(b)
+from collections import deque
 
-    dfs(0, 0, 0, [])
+## bfs
+def solution(info, edges):
+    answer = 0
+    
+    graph = {}
+    for i in range(len(info)):
+        graph[i] = []
+        
+    for edge in edges:
+        parent, child = edge
+        graph[parent].append(child)
+    
+    #print(graph)
+    
+    q = deque([[[0], 1, 0]]) # [nodes, sheep, wolf]
+    while q:
+        #print(q[0])
+        nodes, sheep, wolf = q.popleft()
+        answer = max(answer, sheep)
+        node_set = {node:0 for node in nodes}
+        #print(node_set)
+        for node in nodes:
+            for nxt in graph[node]:
+                n_sheep, n_wolf = sheep, wolf
+                if nxt not in node_set:
+                    if info[nxt] == 0:
+                        n_sheep += 1
+                    else:
+                        n_wolf += 1
+                    
+                    if n_sheep > n_wolf:
+                        q.append([nodes + [nxt], n_sheep, n_wolf])
+    
+    
     return answer
 
-# from collections import deque
-
+## dfs
 # def solution(info, edges):
 #     answer = 0
-#     queue = deque()
-#     queue.append(0)
-    
-#     graph = {i:[] for i in range(len(info))}
-#     for parent, children in edges:
-#         graph[parent].append(children)
-        
-#     visited = [False] * len(info)
-#     sheep, wolf = 0, 0
-#     while queue:
-#         print(f'queue: {queue}')
-#         node = queue.popleft()
-#         if not visited[node]:
-#             visited[node] = True
-#             if node == 0:
-#                 sheep += 1
-#             else:
-#                 wolf += 1
-#                 if sheep == wolf:
-#                     wolf -= 1
-#                     queue.append(parent)
-#                     visited[node] = False
-#                     continue
-#             try:
-#                 for j in graph[node]:
-#                     parent = node
-#                     queue.append(j)
-#             except:
-#                 print(node, 'error')
-    
-#     return sheep
