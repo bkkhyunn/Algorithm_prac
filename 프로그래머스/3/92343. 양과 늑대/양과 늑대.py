837603034d26e0db94a04bce30b809e168040eca
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-05-11 00:36:55 +0900

    [level 3] Title: 양과 늑대, Time: 0.36 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,41 +1,62 @@
 from collections import deque
 
 ## bfs
-def solution(info, edges):
-    answer = 0
-    
-    graph = {}
-    for i in range(len(info)):
-        graph[i] = []
-        
-    for edge in edges:
-        parent, child = edge
-        graph[parent].append(child)
-    
-    #print(graph)
-    
-    q = deque([[[0], 1, 0]]) # [nodes, sheep, wolf]
-    while q:
-        #print(q[0])
-        nodes, sheep, wolf = q.popleft()
-        answer = max(answer, sheep)
-        node_set = {node:0 for node in nodes}
-        #print(node_set)
-        for node in nodes:
-            for nxt in graph[node]:
-                n_sheep, n_wolf = sheep, wolf
-                if nxt not in node_set:
-                    if info[nxt] == 0:
-                        n_sheep += 1
-                    else:
-                        n_wolf += 1
-                    
-                    if n_sheep > n_wolf:
-                        q.append([nodes + [nxt], n_sheep, n_wolf])
-    
-    
-    return answer
-
-## dfs
 # def solution(info, edges):
 #     answer = 0
+    
+#     graph = {}
+#     for i in range(len(info)):
+#         graph[i] = []
+        
+#     for edge in edges:
+#         parent, child = edge
+#         graph[parent].append(child)
+    
+#     #print(graph)
+    
+#     q = deque([[[0], 1, 0]]) # [nodes, sheep, wolf]
+#     while q:
+#         #print(q[0])
+#         nodes, sheep, wolf = q.popleft()
+#         answer = max(answer, sheep)
+#         node_set = {node:0 for node in nodes}
+#         #print(node_set)
+#         for node in nodes:
+#             for nxt in graph[node]:
+#                 n_sheep, n_wolf = sheep, wolf
+#                 if nxt not in node_set:
+#                     if info[nxt] == 0:
+#                         n_sheep += 1
+#                     else:
+#                         n_wolf += 1
+                    
+#                     if n_sheep > n_wolf:
+#                         q.append([nodes + [nxt], n_sheep, n_wolf])
+    
+    
+#     return answer
+
+# dfs
+def solution(info, edges):
+    visited = [0] * len(info)
+    answer = []
+    
+    def dfs(sheep, wolf):
+        if sheep > wolf:
+            answer.append(sheep)
+        else:
+            return 
+        
+        for p, c in edges:
+            if visited[p] and not visited[c]:
+                visited[c] = 1
+                if info[c] == 0:
+                    dfs(sheep+1, wolf)
+                else:
+                    dfs(sheep, wolf+1)
+                visited[c] = 0
+                
+    visited[0] = 1
+    dfs(1, 0)
+
+    return max(answer)
