bda2116bc7dc9024cc98b7589b95ea2da3fd6c0c
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-05-13 17:00:08 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,22 +1,47 @@
 from collections import deque
 
 # bfs
+# def solution(n, computers):
+#     answer = 1
+#     queue = deque([0])
+#     visited = [False] * n
+#     while queue:
+#         idx = queue.popleft()
+#         visited[idx] = True
+#         for i, computer in enumerate(computers[idx]):
+#             if (not visited[i]) and computer == 1:
+#                 queue.append(i)
+#         if len(queue) == 0 and (False in visited):
+#             answer += 1
+#             for i, visit in enumerate(visited):
+#                 if not visit:
+#                     queue.append(i)
+#                     break
+#     return answer
+
+# bfs 좀 더 깔끔한 풀이
 def solution(n, computers):
-    answer = 1
-    queue = deque([0])
-    visited = [False] * len(computers)
-    while queue:
-        idx = queue.popleft()
-        visited[idx] = True
-        for i, computer in enumerate(computers[idx]):
-            if (not visited[i]) and computer == 1:
-                queue.append(i)
+    answer = 0
+    visited = [False] * n
+    
+    def bfs(computers, start, visited):
+        queue = deque([start])
+        # 현재 노드 방문 처리
+        visited[start] = True
         
-        if not len(queue) and (False in visited):
-            answer += 1
-            for i, visit in enumerate(visited):
-                if not visit:
+        while queue:
+            # 현재 노드
+            v = queue.popleft()
+            for i in range(n):
+                # 현재 노드와 연결되어 있고, 방문하지 않은 노드이면 방문 처리 하고 큐에 삽입
+                if computers[v][i] == 1 and not visited[i]:
+                    visited[i] = True
                     queue.append(i)
-                    break
-        
+    
+    # 방문 하지 않은 노드들을 돌아가면서 bfs
+    for i in range(n):
+        if not visited[i]:
+            answer += 1
+            bfs(computers, i, visited)
+            
     return answer
