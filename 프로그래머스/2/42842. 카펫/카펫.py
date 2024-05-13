19cf6d6fb168e0689fc3623c025668d6265fe473
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-05-14 00:41:17 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,13 @@
+def solution(brown, yellow):
+    answer = []
+    total_grid = brown + yellow
+    cand = []
+    for i in range(1, (int(yellow**(1/2)) + 1)):
+        if yellow % i == 0:
+            cand.append((yellow // i, i))
+    #print(cand)
+    for w, h in cand:
+        if (w+2) * (h+2) == total_grid:
+            answer = [w+2, h+2]
+        
+    return answer
