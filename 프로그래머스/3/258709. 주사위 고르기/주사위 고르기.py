ad6664aa6b45e28a848ad4ea5dd76769c0355169
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-05-18 01:15:53 +0900

    [level 3] Title: 주사위 고르기, Time: 1603.41 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -0,0 +1,36 @@
+from itertools import combinations, product
+from bisect import bisect_left
+
+def solution(dice):
+    answer = []
+    
+    # a, b 주사위 선택
+    n = len(dice)
+    a_dice = list(combinations(range(n), n // 2))
+    b_dice = list(combinations(range(n), n // 2))[::-1]
+    
+    max_win, a_win = 0, 0
+    for a, b in zip(a_dice, b_dice):
+        a_dices = list(map(lambda x: dice[x], a))
+        b_dices = list(map(lambda x: dice[x], b))
+        a_res, b_res = [], []
+        for i in product(*a_dices):
+            a_res.append(sum(i))
+        for i in product(*b_dices):
+            b_res.append(sum(i))
+        b_res.sort()
+        l = len(b_res)
+        
+        for i in a_res:
+            r = bisect_left(b_res, i)
+            #print(r)
+            a_win += r
+            
+        if max_win < a_win:
+            max_win = a_win
+            answer = sorted(map(lambda x: x+1, [*a]))
+            a_win = 0
+            
+        a_win = 0
+        
+    return answer
