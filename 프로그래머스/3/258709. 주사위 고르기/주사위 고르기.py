91aea173c5d41b232fc4b8e3a820346405caeb79
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-05-18 01:17:36 +0900

    [level 3] Title: 주사위 고르기, Time: 1608.17 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -9,15 +9,18 @@ def solution(dice):
     a_dice = list(combinations(range(n), n // 2))
     b_dice = list(combinations(range(n), n // 2))[::-1]
     
+    # 승 수 구하기. bisect 를 통해 시간 감소시키기
     max_win, a_win = 0, 0
     for a, b in zip(a_dice, b_dice):
         a_dices = list(map(lambda x: dice[x], a))
         b_dices = list(map(lambda x: dice[x], b))
         a_res, b_res = [], []
+        
         for i in product(*a_dices):
             a_res.append(sum(i))
         for i in product(*b_dices):
             b_res.append(sum(i))
+            
         b_res.sort()
         l = len(b_res)
         
