ecb9c3029b5c0dfce4fbf3f05eac704286fc9a46
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-05-22 01:40:56 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,25 +1,25 @@
-# def solution(citations):
-#     answer = 0
-#     for i in range(len(citations)+1):
-#         h = i
-#         c_paper, n_paper = 0, 0
-#         for citation in citations:
-#             if citation >= h:
-#                 c_paper += 1
-#             else:
-#                 n_paper += 1
-        
-#         if c_paper >= h and n_paper <= h:
-#             answer = max(answer, h)
-            
-#     return answer
-
-
-# 다른 풀이
+# 완전 탐색
 def solution(citations):
-    citations = sorted(citations)
-    l = len(citations)
-    for i in range(l):
-        if citations[i] >= l-i:
-            return l-i
-    return 0
+    answer = 0
+    for i in range(len(citations)+1):
+        h = i
+        c_paper, n_paper = 0, 0
+        for citation in citations:
+            if citation >= h:
+                c_paper += 1
+            else:
+                n_paper += 1
+        
+        if c_paper >= h and n_paper <= h:
+            answer = max(answer, h)
+            
+    return answer
+
+# # 정렬
+# def solution(citations):
+#     citations.sort(reverse=True)
+#     for i in range(len(citations)):
+#         if citations[i] <= i+1:
+#             return max(citations[i], i)
+#     else:
+#         return len(citations) 
