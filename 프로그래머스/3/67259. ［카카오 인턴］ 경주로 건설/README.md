b7a182ec4a7c5fd4f24289705213bb49a15ac34b
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-05-30 22:05:21 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.07 ms
+메모리: 10.4 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 30일 02:27:10
+2024년 05월 30일 22:05:19
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,4 +1,10 @@
-# dfs - stack
+# dfs - stack + DP
+# 탐색 방향(up, down, left, right)의 조합에 따라 정답이 될수도 안될수도 있는 문제이다.
+# 다익스트라로는 문제가 해결되지 않는다. 가장 비용이 적은 길을 골랐는데, 최종 목적지까지 코너가 생기면 결국엔 더 큰 비용이
+# 될 수도 있기 때문이다.
+# 따라서 좌표별로 cost 를 같이 기록하고, 기록된 cost 가 해당 좌표로 도달할 수 있는 방향만큼 모두 기록하도록 해서
+# 모든 경우를 구하도록 하면 완벽한 정답을 구할 수 있을 것 같다.
+
 def solution(board):
     
     # board 에 cost 까지 넣어두기
@@ -28,7 +34,7 @@ def solution(board):
             if nx < 0 or ny < 0 or nx >= len(board) or ny >= len(board):
                 continue
             
-            # 방문하지 않았거나, 기존 cost 보다 새로운 cost 가 더 쌀 때
+            
             if direct == '':
                 if not board[nx][ny][0]:
                     board[nx][ny] = [1, cost+100]
@@ -40,6 +46,7 @@ def solution(board):
                         costs.append(cost+600)
                         board[x][y][1] = 1000000
                         continue
+                    # 방문하지 않았거나, 기존 cost 보다 새로운 cost 가 더 쌀 때
                     if not board[nx][ny][0] or board[nx][ny][1] >= cost+600:
                         board[nx][ny] = [1, cost+600]
                         stack.append((nx, ny, cost+600, ndirect))
@@ -49,6 +56,7 @@ def solution(board):
                         costs.append(cost+100)
                         board[x][y][1] = 10000000
                         continue
+                    # 방문하지 않았거나, 기존 cost 보다 새로운 cost 가 더 쌀 때
                     if not board[nx][ny][0] or board[nx][ny][1] >= cost+100: 
                         board[nx][ny] = [1, cost+100]
                         stack.append((nx, ny, cost+100, ndirect))
