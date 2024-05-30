68525faa1f706b253ff561c3785e6613d34fab91
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-05-30 21:56:15 +0900

    [level 3] Title: 불량 사용자, Time: 0.38 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.63 ms
+메모리: 10.4 MB, 시간: 0.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 30일 21:50:23
+2024년 05월 30일 21:56:13
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,13 +1,16 @@
 import re
 from itertools import product
 
+# 구현 문제인 것 같다.
 def solution(user_id, banned_id):
     answer = 0
     possibles = []
     
     for ban in banned_id:
-        cnt = 0
+        # 불량 사용자 아이디 목록을 정규식을 사용할 수 있게 패턴화 한다.
         pattern = ban.replace('*', '[a-z0-9]')
+        
+        # 불량 사용자 아이디 한 개에 해당할 수 있는 유저 아이디를 담는다.
         possible = []
         for user in user_id:
             if [user] == re.findall(pattern, user):
@@ -16,6 +19,8 @@ def solution(user_id, banned_id):
         if possible:
             possibles.append(possible)
     
+    #print(possibles)
+    # 순서에 따라 중복을 제거해주기 위해서 아래 과정을 거친다.
     candidate = []
     for cand in product(*possibles):
         if len(cand) == len(set(cand)):
