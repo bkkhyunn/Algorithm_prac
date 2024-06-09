2569fcd2070d99e1596769ea9b41040730ddc6b8
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-09 21:44:07 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 43.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -4,12 +4,14 @@ from collections import Counter
 
 def solution(n, wires):
     answer = 999999
-        
-    def find(parent, idx):
-        if parent[idx] != idx:
-            parent[idx] = find(parent, parent[idx])
-        return parent[idx]
-        
+    
+    # 특정 노드가 속한 집합 찾기
+    def find(parent, x):
+        if parent[x] != x:
+            parent[x] = find(parent, parent[x])
+        return parent[x]
+    
+    # 두 노드가 속한 집합 합치기
     def union(parent, a, b):
         a = find(parent, a)
         b = find(parent, b)
@@ -18,18 +20,22 @@ def solution(n, wires):
         else:
             parent[a] = b
     
+    # wires 에서 하나씩 끊으며 완전탐색
     for i in range(len(wires)):
         n_wires = copy.deepcopy(wires)
         n_wires.pop(i)
         
+        # 부모 테이블 초기화
         parent = [0] * (n+1)
         for j in range(1, n+1):
             parent[j] = j
         
+        # 간선 정보를 가지고 Union 수행
         for a, b in n_wires:
             union(parent, a, b)
         
-        for idx in range(len(parent)):
+        # 부모 테이블 자체에는 부모 노드만 설정되어 있기 때문에 루트 노드로 갱신이 필요하다.
+        for idx in range(1, n+1):
             find(parent, idx)
         
         count = Counter(parent[1:])
