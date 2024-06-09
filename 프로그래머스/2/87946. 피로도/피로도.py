16ec38f8ee9d3c095ebe6fd77557906e455aaf94
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-09 19:14:57 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,3 +1,4 @@
+# 완전 탐색 - 순열
 from itertools import permutations
 
 def solution(k, dungeons):
@@ -16,4 +17,26 @@ def solution(k, dungeons):
                 
         answer = max(answer, clear)
         
+    return answer
+
+# 완전 탐색 - DFS, 백트래킹
+def solution(k, dungeons):
+    answer = 0
+    visited = [False] * len(dungeons)
+    
+    def dfs(k, cnt, dungeons):
+        nonlocal answer, visited
+        
+        if cnt > answer:
+            answer = cnt
+
+        for idx in range(len(dungeons)):
+            
+            if k >= dungeons[idx][0] and not visited[idx]:
+                visited[idx] = True
+                dfs(k - dungeons[idx][1], cnt+1, dungeons)
+                # 백트래킹
+                visited[idx] = False
+                
+    dfs(k, 0, dungeons)
     return answer
