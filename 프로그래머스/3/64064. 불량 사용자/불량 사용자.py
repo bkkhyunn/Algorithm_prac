9dbba9c75c4124d332a5974153420b156971f2b9
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-09 15:25:25 +0900

    [level 3] Title: 불량 사용자, Time: 0.38 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -32,23 +32,23 @@ def solution(user_id, banned_id):
 
 # 두번째 풀이 (개선)
 def solution(user_id, banned_id):
-
-    banned_id = ["'" + _.replace('*', '[a-z0-9]') + "'"  for _ in banned_id]
-    #print(f'banned_id 정규식 전환 : {banned_id}')
-    
+    answer = 0
     s_user_id = str(user_id)
     #print(f'user_id 문자열 전환 : {s_user_id}')
     
-    possible_list = [re.findall(_, s_user_id) for _ in banned_id]   
+    possible_list = []
+    for ban_id in banned_id:
+        pattern = "'" + ban_id.replace('*', '[a-z0-9]') + "'"
+        possible_list.append(re.findall(pattern, s_user_id))
     #print(f'banned_id 별 가능한 user_id : {possible_list}')
     
-    possible_list = list(product(*possible_list))
-    #print(f'banned_id 별 가능한 user_id 의 모든 조합 : {possible_list}')
+    candidate = []
+    for possible in product(*possible_list):
+        # 모든 조합 중 동일 아이디 제거
+        if len(possible) == len(set(possible)):
+            # 모든 조합 중 같은 조합 제거
+            if set(possible) not in candidate:
+                candidate.append(set(possible))
+                answer += 1
     
-    possible_list = [frozenset(p) for p in possible_list if len(set(p)) == len(p)]
-    #print(f'banned_id 별 가능한 user_id 의 모든 조합 중 동일 아이디 제거 : {possible_list}')
-    
-    possible_list = set(possible_list)
-    #print(f'banned_id 별 가능한 user_id 의 모든 조합 중 중복 제거 : {possible_list}')
-    
-    return len(possible_list)
+    return answer
