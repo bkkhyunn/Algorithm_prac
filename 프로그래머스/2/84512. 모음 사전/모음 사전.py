e68b5de090ff9562bcb73cdb03fdb228bb66b537
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-09 23:55:57 +0900

    [level 2] Title: 모음 사전, Time: 0.41 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,11 @@
-# 완전탐색 - 중복순열
+## 완전탐색 - 중복순열
 from itertools import product
 
 def solution(word):
     possibles = list()
     spells = ['A','E','I','O','U']
     
+    # 가능한 모든 경우의 수 생성
     for j in range(1,6):
         for w in product(spells,repeat=j):
             possibles.append(''.join(w))
@@ -14,13 +15,28 @@ def solution(word):
             
     return possibles.index(word) + 1
 
-# 완전탐색 - DFS
-# def solution(word):
-#     answer = 0
-#     spells = ['A','E','I','O','U']
+## 완전탐색 - DFS
+def solution(word):
+    answer = 0
+    spells = ['A','E','I','O','U']
     
-#     def dfs():
+    def dfs(spell, word):
+        nonlocal answer, spells
+        answer += 1
         
+        # 만들어진 단어가 주어진 word 와 같으면 True
+        if spell == word:
+            return True
         
-#     return answer
+        if len(spell) == 5:
+            return False
+        
+        for s in spells:
+            if dfs(spell+s, word):
+                return True
+        
+    for spell in spells:
+        if dfs(spell, word):
+            return answer
     
+    return answer   
