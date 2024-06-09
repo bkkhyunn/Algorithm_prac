f2399cf707a1d0a044b8016bab95b312f468a6df
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-09 15:19:03 +0900

    [level 3] Title: 불량 사용자, Time: 0.37 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,7 +1,7 @@
 import re
 from itertools import product
 
-# 구현 문제인 것 같다.
+# 첫번째 풀이 (구현)
 def solution(user_id, banned_id):
     answer = 0
     possibles = []
@@ -29,3 +29,26 @@ def solution(user_id, banned_id):
                 answer += 1
     
     return answer
+
+# 두번째 풀이 (개선)
+def solution(user_id, banned_id):
+
+    banned_id = ["'" + _.replace('*', '[a-z0-9]') + "'"  for _ in banned_id]
+    #print(f'banned_id 정규식 전환 : {banned_id}')
+    
+    s_user_id = str(user_id)
+    #print(f'user_id 문자열 전환 : {s_user_id}')
+    
+    possible_list = [re.findall(_, s_user_id) for _ in banned_id]   
+    #print(f'banned_id 별 가능한 user_id : {possible_list}')
+    
+    possible_list = list(product(*possible_list))
+    #print(f'banned_id 별 가능한 user_id 의 모든 조합 : {possible_list}')
+    
+    possible_list = [frozenset(p) for p in possible_list if len(set(p)) == len(p)]
+    #print(f'banned_id 별 가능한 user_id 의 모든 조합 중 동일 아이디 제거 : {possible_list}')
+    
+    possible_list = set(possible_list)
+    #print(f'banned_id 별 가능한 user_id 의 모든 조합 중 중복 제거 : {possible_list}')
+    
+    return len(possible_list)
