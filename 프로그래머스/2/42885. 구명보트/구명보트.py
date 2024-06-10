5b2a5062eaa396a69fd4a6efbd17087b99421ef4
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-11 01:08:34 +0900

    [level 2] Title: 구명보트, Time: 8.30 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,18 +1,21 @@
+# 구명보트는 작아서 한 번에 최대 2명씩 밖에 탈 수 없고, 무게 제한이 있다.
+# 그렇다면 몸무게가 많이 나가는 사람부터 태우고, 가벼운 사람 중에 태울 수 있는 사람을 태우면 된다.
+
+from collections import deque
+
 def solution(people, limit):
     answer = 0
-    people.sort()      
-    # -> [0,0,0,0] <-
-    start = 0
-    end = len(people) -1
+    people.sort(reverse=True)
+    queue = deque(people)
     
-    while start <= end :     
+    while queue:
+        max_weight = queue.popleft()
+        sum_weight = max_weight
         
-        if (people[start] + people[end]) <= limit:  
-            start += 1
-            end -= 1
-        else:                     
-            end -= 1
-            
-        answer +=1
+        if queue and (sum_weight + queue[-1] <= limit):
+            min_weight = queue.pop()
+            sum_weight += min_weight
+        
+        answer += 1
         
     return answer
