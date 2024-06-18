e87d2ab10092d391cc2ab8f98c27ebf6ab89d3cc
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-18 18:52:29 +0900

    [level 3] Title: 입국심사, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,45 @@
+# 아이디어
+# times 중 가장 오래 걸리는 시간 * 입국 인원 을 하면 가능한 가장 큰 시간이 나온다.
+# 이를 기준으로 이진 탐색을 실시.
+
+def solution(n, times):
+    times.sort()
+    # 시작점, 끝점
+    start = 0
+    end = times[-1] * n
+    
+    def binary_search(start, end):
+        nonlocal times, n
+        
+        cnt = 0
+        answer = -1
+
+        while start <= end:
+            mid = int((start+end)/2)
+            cnt = 0
+
+            # mid 시간 안에 각 심사관 별로 해당 시간에 몇 명을 처리할 수 있는지
+            for time in times:
+                cnt += int(mid/time)
+            
+            # 입국 심사 인원보다 더 많이 처리할 때
+            if cnt >= n:
+                
+                # 정답 최신화
+                if answer == -1:
+                    answer = mid
+                else:
+                    answer = min(answer,mid)
+                
+                # 끝 시간을 중간 시간보다 1 작게 최신화
+                end = mid - 1
+            
+            # 입국 심사 인원보다 더 적게 처리할 때
+            else:
+                # 시작 시간을 중간 시간보다 1 크게 최신화
+                start = mid + 1
+
+        return answer
+    
+    answer = binary_search(start, end)
+    return answer
