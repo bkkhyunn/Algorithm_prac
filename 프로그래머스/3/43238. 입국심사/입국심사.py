2a546df89dd797b4b478f215a5b02d6a6e6acb2a
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-18 18:55:47 +0900

    [level 3] Title: 입국심사, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,6 +1,7 @@
 # 아이디어
 # times 중 가장 오래 걸리는 시간 * 입국 인원 을 하면 가능한 가장 큰 시간이 나온다.
 # 이를 기준으로 이진 탐색을 실시.
+# 즉 시간을 배열로 보고, 계속해서 중간 시간에 최대 몇 명을 처리할 수 있는지 계산한다.
 
 def solution(n, times):
     times.sort()
@@ -15,12 +16,12 @@ def solution(n, times):
         answer = -1
 
         while start <= end:
-            mid = int((start+end)/2)
+            mid = (start + end) // 2
             cnt = 0
 
             # mid 시간 안에 각 심사관 별로 해당 시간에 몇 명을 처리할 수 있는지
             for time in times:
-                cnt += int(mid/time)
+                cnt += mid // time
             
             # 입국 심사 인원보다 더 많이 처리할 때
             if cnt >= n:
