b746896261c38c07a2ffe19f931fcf112fe4c03f
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-19 23:30:13 +0900

    [level 4] Title: 징검다리, Time: 109.19 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/README.md
+++ 프로그래머스/4/43236. 징검다리/README.md
@@ -1,10 +1,10 @@
 # [level 4] 징검다리 - 43236 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43236#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43236) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 115.64 ms
+메모리: 11.5 MB, 시간: 109.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 19일 23:25:10
+2024년 06월 19일 23:30:11
 
 ### 문제 설명
 
--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,5 +1,5 @@
 # input 의 값이 매우 크기 때문에 이진탐색으로 도전
-# 이진탐색의 기준값은 바위 사이의 거리
+# 이진탐색의 기준값은 바위 사이 거리의 최소값
 # 바위 사이의 거리를 기준으로 점점 줄여가면서, 제거되는 바위의 개수가 n 개일 때 최소값들 중 최대값을 구한다.
 # 제거되는 바위의 개수가 n 개일 때 최솟값들 중 최댓값을 찾는다. 이에 start 를 증가시켜 범위를 높혀
 # 반대 방향으로 탐색하여 최솟값들 중 최댓값을 찾아준다.
@@ -14,7 +14,7 @@ def solution(distance, rocks, n):
     
     while start <= end:
         
-        # 이번 탐색의 바위 사이의 거리
+        # 이번 탐색에서 원하는 바위 사이 거리의 최소값
         mid = (start + end) // 2  
         min_distance = float('inf')  
         current, rm_cnt = 0, 0
@@ -33,7 +33,8 @@ def solution(distance, rocks, n):
             # 바위 사이의 거리가 이번 탐색에서 설정한 바위 사이의 거리보다 크거나 같다면 다음 거리를 구한다.
             # 그리고 최소 거리를 계산할 current 를 갱신한다.
             else:  
-                current = rock  
+                current = rock
+                # 이진탐색에서 설정한 최소거리가 나오지 않을 수 있기 때문에 실제 최소값을 저장한다.
                 min_distance = min(min_distance, diff)  
         
         # n 보다 많이 제거했다면 설정한 최소 거리가 너무 크다면 줄여준다.
