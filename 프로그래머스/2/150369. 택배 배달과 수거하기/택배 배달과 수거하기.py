e3f82b45f9bdfdec13a13051af9a155c16fb9a5d
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-23 01:33:01 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 347.31 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -35,3 +35,42 @@ def solution(cap, n, deliveries, pickups):
         answer += max(len(deliveries), len(pickups)) * 2
     
     return answer
+
+# 개선된 방법
+from itertools import zip_longest
+
+def box_list(l):
+    '''
+    box에 거리를 대응시켜서 box 개수 크기의 list 를 반환
+    
+    ex. [1, 0, 3, 1, 2] -> [1, 3, 3, 3, 4, 5, 5]
+    1번째 집에 box 1개
+    3번째 집에 box 3개
+    4번째 집에 box 1개
+    5번째 집에 box 2개
+    '''
+    n = []
+    for i,d in enumerate(l):
+        for _ in range(d):
+            n.append(i+1)
+    return n
+
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    d_box = box_list(deliveries)
+    p_box = box_list(pickups)
+    
+    # 먼 거리의 box 부터 배달/수거 하기 위해 뒤집기
+    d_box.reverse()
+    p_box.reverse()
+    
+    # cap 개 만큼 트럭에 실을 수 있기 때문에 step 으로 cap
+    d_box = d_box[::cap]
+    p_box = p_box[::cap]
+    
+    # 트럭이 한 번 움직일 때 움직이는 가장 긴 거리
+    # 긴 거리부터 처리하면서 배달/수거 개수를 줄이면 그리디의 정당성이 보장된다.
+    for d, p in zip_longest(d_box, p_box, fillvalue=0):
+        answer += max(d, p) * 2
+    
+    return answer
