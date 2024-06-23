2bee491135b623c2a5b19799844ed75774b1afbb
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-06-23 17:19:08 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 4.62 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -3,7 +3,9 @@ from collections import deque
 def solution(friends, gifts):
     answer = 0
     # (나에게 받은 사람: 선물 개수), (나에게 준 사람: 선물 개수)
-    log = {friend : [{friend : 0 for friend in friends}, {friend : 0 for friend in friends}] for friend in friends}
+    log = {friend : [{friend : 0 for friend in friends},
+                     {friend : 0 for friend in friends}] for friend in friends}
+    # 선물 수 기록
     gift_num = {friend : 0 for friend in friends}
     
     for gift in gifts:
@@ -17,13 +19,11 @@ def solution(friends, gifts):
         
         for other in queue:
             
-            if friend == other:
-                continue
-            
             if log[friend][0][other] > log[other][0][friend]:
                 gift_num[friend] += 1
             
             elif log[friend][0][other] == log[other][0][friend]:
+                # 선물 지수
                 f_n = sum(log[friend][0].values()) - sum(log[friend][1].values())
                 o_n = sum(log[other][0].values()) - sum(log[other][1].values())
                 
@@ -33,6 +33,7 @@ def solution(friends, gifts):
                     continue
                 else:
                     gift_num[other] += 1
+                    
             else:
                 gift_num[other] += 1
     
