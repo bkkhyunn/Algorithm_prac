f51e8e491507542841f88e42b5eb31cee18c8425
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-01 21:12:10 +0900

    [level 3] Title: 산 모양 타일링, Time: 56.84 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/README.md
+++ 프로그래머스/3/258705. 산 모양 타일링/README.md
@@ -1,10 +1,10 @@
 # [level 3] 산 모양 타일링 - 258705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258705) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258705#qna) 
 
 ### 성능 요약
 
-메모리: 15.9 MB, 시간: 55.18 ms
+메모리: 15.8 MB, 시간: 56.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 01일 21:05:55
+2024년 07월 01일 21:12:09
 
 ### 문제 설명
 
--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
@@ -1,23 +1,26 @@
+# 삼각형이 한개씩 생길 때마다 그 전의 경우의 수를 이용해 구할 수 있다. 따라서 DP 로 접근
 # dp 점화식
 # 윗변과 맞닿은 삼각형이 생기는 때는 3의 배수일 때.
 # if (n-4) % 3 == 0 일 때 dp_n = dp_{n-1} + dp_{n-3}
 # if (n-4) % 3 != 0 일 때 dp_n = dp_{n-1} + dp_{n-2}
+# 미리 10007 로 나누어 DP 를 관리해서 메모리를 줄인다.
 
 def solution(n, tops):
 
     total = (2*n+1) + len(tops)
-    
     dp = [0] * (total+1)
     
     for i in range(1, total+1):
         if i <= 2:
             dp[i] = dp[i-1] + 1
             continue
-            
+        
+        # 윗변에 삼각형이 없다면
         if (i-3) % 3 == 0 and tops[(i-3)//3] == 0:
             dp[i] = dp[i-1]
             continue
         
+        # 좌측 하단 삼각형이 생길 때, 그 삼각형을 포함한 마름모는 3단계 전 경우의 수이다.
         if (i-4) % 3 == 0:
             dp[i] = (dp[i-1] + dp[i-3]) % 10007
         else:
