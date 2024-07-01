c89655e680fd8dcf874eecf9a82340a524a11989
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-02 00:33:11 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 33.32 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,25 +1,32 @@
+# 자식은 1인데 부모(루트)가 0 이면 불가능한 경우.
+
 def solution(numbers):
     answer = []
     
+    # 만들 수 있는지 없는지 판단하는 함수
     def check(b):
         
-        if root := (len(b) // 2):
-        
+        if len(b) >= 3:
+            root = (len(b) // 2)
+             # 부모(루트)는 0인데 자식이 1이 하나라도 있는 경우 만들 수 없다.
             if b[root] == '0' and ('1' in b):
                 return False
-        
+            # 부모(루트)가 1인 경우 더 작은 서브트리로 넘어간다.
             else:
                 return check(b[:root]) and check(b[root+1:])
+        # 트리의 길이가 3보다 적으면 확인할 필요 없다.
         else:
             return True
     
     for num in numbers:
         b = bin(num)[2:]
+        
+        # 포화 이진 트리 만들기
         i = 0
         while len(b) > (2**i-1):
             i += 1
-            
-        b = b.rjust((2**i-1), '0')
+        
+        b = b.rjust((2**i-1), '0') # 오른쪽부터 첫번째 인자의 길이만큼 두번째 인자를 채운다.
         #print(b)
         
         answer.append(int(check(b)))
