feb0bfaa0b62cc5ae262650bf02fd4ed728b6ee5
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-02 02:44:31 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -3,6 +3,8 @@ import math
 
 def solution(fees, records):
     answer = []
+    
+    # 차량 번호 별 IN, OUT 시간 담기
     cars = set([record.split()[1] for record in records])
     park_dict = {car:[[],[]] for car in cars}
     
@@ -12,10 +14,13 @@ def solution(fees, records):
             park_dict[num][0].append(t)
         else:
             park_dict[num][1].append(t)
-            
+    
+    # 차량 번호가 적은 것부터
     for num in sorted(park_dict.keys()):
+        # 누적 주차 시간
         park_time = 0
-        for in_info, out_info in zip_longest(park_dict[num][0], park_dict[num][1], fillvalue="23:59"):
+        for in_info, out_info in zip_longest(park_dict[num][0], park_dict[num][1],
+                                             fillvalue="23:59"):
             ih = (int(in_info.split(':')[0])*60) + int(in_info.split(':')[1])
             oh = (int(out_info.split(':')[0])*60) + int(out_info.split(':')[1])
             park_time += (oh - ih)
