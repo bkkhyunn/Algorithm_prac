580a75a19a1312c675478fdd5d048b6890eb7ac5
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-02 02:41:58 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -0,0 +1,28 @@
+from itertools import zip_longest
+import math
+
+def solution(fees, records):
+    answer = []
+    cars = set([record.split()[1] for record in records])
+    park_dict = {car:[[],[]] for car in cars}
+    
+    for record in records:
+        t, num, info = record.split()
+        if info == 'IN':
+            park_dict[num][0].append(t)
+        else:
+            park_dict[num][1].append(t)
+            
+    for num in sorted(park_dict.keys()):
+        park_time = 0
+        for in_info, out_info in zip_longest(park_dict[num][0], park_dict[num][1], fillvalue="23:59"):
+            ih = (int(in_info.split(':')[0])*60) + int(in_info.split(':')[1])
+            oh = (int(out_info.split(':')[0])*60) + int(out_info.split(':')[1])
+            park_time += (oh - ih)
+        
+        if park_time <= fees[0]:
+            answer.append(fees[1])
+        else:
+            answer.append(fees[1] + (math.ceil((park_time - fees[0]) / fees[2]) * fees[3]))
+        
+    return answer
