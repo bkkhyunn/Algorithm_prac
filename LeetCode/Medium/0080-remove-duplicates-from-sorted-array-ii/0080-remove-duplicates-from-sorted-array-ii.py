425fc9be167857b1e014b03252a0e99de676aceb
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-03 15:42:31 +0900

    Time: 56 ms (30.35%), Space: 16.7 MB (14.99%) - LeetHub

--- LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -11,3 +11,13 @@ class Solution:
                 count_dict[n] -= 1
         
         return len(nums)
+    
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        '''two pointer'''
+        j = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[j - 2]:
+                nums[j] = nums[i]
+                j += 1
+        return j
