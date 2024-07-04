2fe88656d81337a09dd0c1a936b0614ba943efd4
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-05 02:11:25 +0900

    Time: 60 ms (42.03%), Space: 17.8 MB (42.25%) - LeetHub

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -25,3 +25,13 @@ class Solution:
         total_profit += sub_profit
                 
         return total_profit
+    
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        '''greedy'''
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i]-prices[i-1]
+        return profit
+    
