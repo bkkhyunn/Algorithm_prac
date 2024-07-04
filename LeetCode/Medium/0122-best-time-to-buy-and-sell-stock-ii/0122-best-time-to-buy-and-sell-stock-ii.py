861155ea0fd51d0e4f168c158cf485b35083bafc
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-05 02:06:41 +0900

    Time: 60 ms (42.03%), Space: 17.9 MB (10.42%) - LeetHub

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,17 +1,27 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        '''
+        dynamic programming & greedy
+        '''
         buy = 10 ** 4
-        max_profit = 0
+        sub_profit = 0
         total_profit = 0
+        
         for price in prices:
             buy = min(price, buy)
             profit = price - buy
-            if max_profit > profit:
-                total_profit += max_profit
-                max_profit = 0
-                buy = price
+            
+            # 그리디에 따라, 얻을 수 있는 이익이 최대치일 때 가져간다. 하루 지나면 못 가져가기 때문.
+            if sub_profit > profit:
+                # 지금까지 얻을 수 있는 최대 이익을 챙기기
+                total_profit += sub_profit
+                # 부분 최대 이익과 구매 가격 재설정
+                sub_profit, buy = 0, price
+
             else:
-                max_profit = max(max_profit, profit)
-        total_profit += max_profit
+                sub_profit = max(sub_profit, profit)
+        
+        # 마지막에 총 이익에 부분 최대 이익 더해주기
+        total_profit += sub_profit
                 
         return total_profit
