91e664eb12fab1f92439de12d12f35f1b54fc527
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-10 22:06:34 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 4.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -8,6 +8,7 @@ def solution(alp, cop, problems):
     problems = [[0,0,1,0,1], [0,0,0,1,1]] + problems
     dp = [[1e9] * 151 for _ in range(151)]
     
+    # 기존의 alp, cop 으로 모든 문제를 푸는 경우가 있을 수 있다.
     max_alp = max(alp, max([p[0] for p in problems]))
     max_cop = max(cop, max([p[1] for p in problems]))
     
@@ -20,15 +21,22 @@ def solution(alp, cop, problems):
         
         now_cost, now_alp, now_cop = heapq.heappop(queue)
         
-        # 이 때 break 를 하게 되면, 우선순위큐 특징에 의해서 모든 문제를 풀 수 있으면서 가장 cost 가 낮다.
+        # 이 때 break 를 하게 되면, 우선순위큐 특징에 의해서 dp[now_alp][now_cop]으로
+        # 모든 문제를 풀 수 있으면서 가장 낮은 cost 를 얻을 수 있다.
         if now_alp >= max_alp and now_cop >= max_cop:
             break
+            
+        if dp[now_alp][now_cop] < now_cost:
+            continue
         
         for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
+            
             # 풀 수 있는 문제인 경우
             if now_alp >= alp_req and now_cop >= cop_req:
                 
-                # dp 테이블의 범주를 넘는 경우. 그 경우에는 최대범위를 갱신해주면 된다.
+                # 예를 들어 max_alp, max_cop 이 150, 150 인 경우
+                # 문제를 하나 풀어 new_alp, new_cop 이 151, 154 같이 될 수 있다.
+                # 그 경우에는 최대범위(150, 150)를 갱신해주면 된다.
                 new_alp = min(now_alp + alp_rwd, 150)
                 new_cop = min(now_cop + cop_rwd, 150)
                 
