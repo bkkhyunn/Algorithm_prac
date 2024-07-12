9eac770c9b5a0e716251202b46e1aa6a44841c7e
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-12 18:00:43 +0900

    Time: 51 ms (14.68%), Space: 17.1 MB (5.15%) - LeetHub

--- LeetCode/Medium/0274-h-index/0274-h-index.py
+++ LeetCode/Medium/0274-h-index/0274-h-index.py
@@ -1,30 +1,56 @@
-import bisect
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        '''
+        정렬 후 선형 탐색
+        '''
+        citations.sort(reverse=True)
+        n = len(citations) # h 의 최대값
+        i, answer = 0, 0
+        
+        # i+1 은 h 이상 인용된 논문의 개수, citations[i] 는 인용된 횟수 h
+        while i+1 <= n and i+1 <= citations[i]:
+            answer = i+1
+            i+=1
+            
+        return answer 
 
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort()
-        h = 0
+        '''
+        Binary Search
+        '''
+        citations.sort(reverse=True)
+
         n = len(citations)
-        answer = 0
+        left, right = 0, n - 1
         
+        while left <= right:
+            mid = left + (right - left) // 2
+            if citations[mid] == mid:
+                return mid
+            elif citations[mid] > mid:
+                left = mid + 1
+            else:
+                right = mid - 1
         
-        while h < max(citations):
-            
-            idx = bisect.bisect_left(citations, h)
-            if n - idx >= h:
-                answer = max(answer, h)
-            #print(h, answer)
-            h += 1
-            
-        return answer
+        return left
     
-
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+        '''
+        Counting Sort
+        '''
         n = len(citations)
-        citations.sort()
+        count = [0]*(n+1)
 
-        for i,v in enumerate(citations):
-            if n - i <= v:
-                return n - i
-        return 0
+        for citation in citations:
+            if citation>=n:
+                count[n]+=1
+            else:
+                count[citation]+=1
+        
+        total  = 0
+        for i in range(n, -1, -1):
+            total += count[i]
+            if total >= i:
+                return i
