14b5c9b7d773bf5daca8c070d665e463b18379cc
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-14 02:57:35 +0900

    [level 3] Title: 사라지는 발판, Time: 239.12 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
@@ -13,7 +13,7 @@ def solution(board, aloc, bloc):
         dy = [0, 0, -1, 1]
         
         if turn % 2 == 0: # A 의 차례
-                
+            # 움직일 수 있는지 판단
             x, y = aloc
             move = []
             for i in range(4):
@@ -26,23 +26,26 @@ def solution(board, aloc, bloc):
                 else:
                     move.append([nx, ny])
             
+            # A 차례일 때 움직일 수 없으면 A 가 지는 것.
             if not move:
                 return (not turn % 2 == 0), turn
-            
+            # A 차례일 때 A 와 B 의 위치가 같으면 A 가 이기는 것.
             if aloc == bloc:
                 return (turn % 2 == 0), turn + 1
             
             board[x][y] = 0
             for nx, ny in move:
-                is_a_win, cnt = dfs(board, [nx, ny], bloc, turn+1)
+                is_a_win, cnt = dfs(board, [nx, ny], bloc, turn+1) # 다음 B 차례의 결과
                 if is_a_win:
+                    # B 가 지는 결과가 나오면 A 는 이긴다.
                     win.add(cnt)
                 else:
+                    # B 가 이기는 결과가 나오면 A 는 진다.
                     lose.add(cnt)
             board[x][y] = 1
             
         else: # B 차례
-                
+            # 움직일 수 있는지 판단
             x, y = bloc
             move = []
             for i in range(4):
@@ -54,22 +57,25 @@ def solution(board, aloc, bloc):
                     continue
                 else:
                     move.append([nx, ny])
-            
+            # B 차례일 때 움직일 수 없으면 A 가 이기는 것
             if not move:
                 return (not turn % 2 == 0), turn
-            
+            # B 차례일 때 A, B 위치가 같으면 A 가 지는 것
             if aloc == bloc:
                 return (turn % 2 == 0), turn + 1
             
             board[x][y] = 0
             for nx, ny in move:
-                is_a_win, cnt = dfs(board, aloc, [nx, ny], turn+1)
+                is_a_win, cnt = dfs(board, aloc, [nx, ny], turn+1) # 다음 A 차례의 결과
                 if is_a_win:
+                    # A 가 이기는 결과가 나오면, B 는 진다.
                     lose.add(cnt)
                 else:
+                    # A 가 지는 결과가 나오면, B 는 이긴다.
                     win.add(cnt)
             board[x][y] = 1
-            
+        
+        # A 혹은 B가 이길 수 밖에 없는지, 질 수 밖에 없는지를 판단한다.
         if win:                           
             return turn % 2 == 0, min(win)
         else:                             
