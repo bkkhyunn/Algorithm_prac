12c2f19c043717c0a1fe115711f0cb7f6378bfc1
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-14 15:27:27 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 106.73 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -2,6 +2,7 @@ import math
 import re
 
 def is_prime_number(x):
+    '''소수 판별 함수'''
     for i in range(2, int(math.sqrt(x))+1):
         if x % i == 0:
             return False
@@ -9,17 +10,24 @@ def is_prime_number(x):
 
 def solution(n, k):
     answer = []
+    
+    # 진수 변환
     trans = ''
     while n > 0:
         n, mod = divmod(n, k)
         trans += str(mod)
     trans = trans[::-1]
     
-    patterns = ['([1-9]+0)', '(0[1-9]+)', '(0[1-9]+0)', '([1-9]+)']
+    # 조건에 맞는 소수 찾기
+    patterns = ['([1-9]+0)', '(0[1-9]+)', '([1-9]+)']
+    
     for pattern in patterns:
+        
         finds = re.findall(pattern, trans)
+        
         for find in finds:
             num = find.replace('0', '')
+            
             if num and is_prime_number(int(num)) and int(num) != 1:
                 answer.append(find)
                 trans = trans.replace(find, '', 1)
