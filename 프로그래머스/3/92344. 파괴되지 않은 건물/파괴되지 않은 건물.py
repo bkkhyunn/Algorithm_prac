972662b8c822321b75639f1fea7710cd3fd2f5f9
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-16 21:45:37 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 1908.75 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,4 +1,6 @@
 # 누적합 사용
+# 효율성 테스트를 통과하기 위해서 값을 갱신하는 부분의 시간 복잡도를 줄여야 한다.
+# 이를 위해 O(NM) 이 걸릴 수 있던 시간 복잡도를 누적합을 사용해서 O(1) 로 줄인다.
 
 import numpy as np
 
@@ -16,11 +18,13 @@ def solution(board, skill):
         prefix_sum[r1][c2+1] -= degree
         prefix_sum[r2+1][c1] -= degree
         prefix_sum[r2+1][c2+1] += degree
-        
+    
+    # 행방향 누적합
     for i in range(n):
         for j in range(1, m):
             prefix_sum[i][j] += prefix_sum[i][j-1]
-        
+    
+    # 열방향 누적합
     for j in range(m):
         for i in range(1, n):
             prefix_sum[i][j] += prefix_sum[i-1][j]
