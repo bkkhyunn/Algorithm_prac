cc982bdea0ad9c9a4f5617fb24747c9a064cc2f6
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-24 22:50:27 +0900

    [Gold III] Title: 파티, Time: 1200 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,7 +1,10 @@
 import heapq
 
+# n : 노드 개수, m : 간선 개수
 n, m, x = map(int, input().split())
+# 단방향
 graph = [[] for _ in range(n+1)]
+# 최소 거리 중 최댓값
 answer = 0
 
 for _ in range(m):
@@ -12,6 +15,7 @@ def dijkstra(start):
     distance = [1e9] * (n+1)
     queue = []
     
+    # (cost, node) 식으로 우선순위큐 삽입
     heapq.heappush(queue, (0, start))
     distance[start] = 0
     
@@ -19,15 +23,16 @@ def dijkstra(start):
         
         cost, now = heapq.heappop(queue)
         
+        # 방문한 적이 있으면 넘어가기
         if distance[now] < cost:
             continue
         
+        # graph -> (next_node, cost)
         for i in graph[now]:
             dist = cost + i[1]
             
             if dist < distance[i[0]]:
                 distance[i[0]] = dist
-                
                 heapq.heappush(queue, (dist, i[0]))
                 
     return distance
