6ca6e42f1ab2fb63b3fff7fa4d660ca687b05d67
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2024-07-25 23:23:17 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 812 ms, Memory: 110488 KB -BaekjoonHub

--- 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.py
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.py
@@ -2,23 +2,16 @@ import sys
 input = sys.stdin.readline
 
 r, c, q = map(int, input().split())
-picture = [[0] * (c+1)]
+picture = [list(map(int, input().split())) for _ in range(r)]
+prefix = [[0] * (c+1) for _ in range(r+1)]
 
-for _ in range(r):
-    prefix_sum = [0]
-    sum_value = 0
-    data = list(map(int, input().split()))
-    for i in data:
-        sum_value += i
-        prefix_sum.append(sum_value)
-    
-    picture.append(prefix_sum)
-    
+# 누적합 행렬 만들기
+for i in range(1, r+1):
+    for j in range(1, c+1):
+        prefix[i][j] = picture[i-1][j-1] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1]
+
 for _ in range(q):
-    r1, c1, r2, c2 = map(int, input().split())
-    cnt = (r2-r1+1) * (c2-c1+1)
-    total = 0
-    for i in range(r1, r2+1):
-        total += (picture[i][c2] - picture[i][c1-1 if c1 > 0 else 0])
+    r1, c1, r2, c2 = map(lambda x: int(x), input().split())
+    total = prefix[r2][c2] - prefix[r1-1][c2] - prefix[r2][c1-1] + prefix[r1-1][c1-1]
+    print(total // ((r2-r1+1) * (c2-c1+1)))
         
-    print(total // cnt)
