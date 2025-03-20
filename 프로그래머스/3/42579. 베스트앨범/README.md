b1d774bd7cdb042e738401dd8942640a12548fcf
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2025-03-20 23:51:15 +0900

    [level 3] Title: 베스트앨범, Time: 0.11 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 9.23 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 07일 18:45:01
+2025년 03월 20일 23:51:13
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,11 +1,20 @@
+from collections import defaultdict
 def solution(genres, plays):
     answer = []
-    playlist = {genre:{} for genre in set(genres)}
-    total = {genre:0 for genre in set(genres)}
-    for i, (g, p) in enumerate(zip(genres, plays)):
-        playlist[g][i] = p
-        total[g] += p
-    for g in sorted(total, key=total.get, reverse=True):
-        for i in sorted(playlist[g], key=playlist[g].get, reverse=True)[:2]:
-            answer.append(i)
+    playlist = defaultdict(list)
+    
+    for i, (genre, play) in enumerate(zip(genres, plays)):
+        if genre not in playlist:
+            playlist[genre] = [play, (i, play)]
+        else:
+            playlist[genre][0] += play
+            playlist[genre].append((i, play))
+    
+    while playlist:
+        genre = max(playlist, key=lambda x: playlist[x][0])
+        #print(playlist[genre][1:])
+        
+        answer += [value[0] for i, value in enumerate(sorted(playlist[genre][1:], key=lambda x: x[1], reverse=True)) if i < 2]
+        
+        del playlist[genre]
     return answer
