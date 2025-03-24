2d5e04c35517c6abf00e89972d9dd9dc11d2e70e
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2025-03-24 23:23:28 +0900

    [level 2] Title: 더 맵게, Time: 1646.31 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,30 +1,28 @@
 import heapq
 
 def solution(scoville, K):
-    blending = 0
+    answer = 0
     heapq.heapify(scoville)
     
-    # scoville 의 제일 작은 수와 그 다음 수가 둘 다 0 일 때
-    if scoville[0] == 0 and scoville[1] == 0:
+    if scoville[0] > K:
+        return answer
+    
+    if scoville[0] + scoville[1] == 0:
         return -1
     
-    while True:
+    while len(scoville) > 1:
+        min1 = heapq.heappop(scoville)
+        min2 = heapq.heappop(scoville)
         
-        if len(scoville) == 1 and scoville[0] < K:
-            return -1
-        
-        if len(scoville) == 1 and scoville[0] >= K:
+        if min1 >= K:
             break
         
-        first = heapq.heappop(scoville)
-        second = heapq.heappop(scoville)
-        
-        if first >= K and second >= K:
-            break
-        
-        new = first + (second * 2)
-        
-        heapq.heappush(scoville, new)
-        blending += 1
+        new_value = min1 + (min2 * 2)
+        heapq.heappush(scoville, new_value)
+            
+        answer += 1
     
-    return blending
+    if len(scoville) and scoville[0] < K:
+        return -1
+    
+    return answer
