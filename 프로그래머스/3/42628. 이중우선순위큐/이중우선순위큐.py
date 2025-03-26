2a1110abd0240e2ceec605bb8d6d325482cf44db
Bkkhyunn <118159810+bkkhyunn@users.noreply.github.com>
2025-03-26 22:39:54 +0900

    [level 3] Title: 이중우선순위큐, Time: 328.22 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,25 +1,18 @@
 import heapq
 
 def solution(operations):
-    
     heap = []
-    for operation in operations:
-        
-        command, data = operation.split()
-        #print(heap)
-        if command == 'I':
-            heapq.heappush(heap, int(data))
-        else:
-            if heap:
-                if int(data) == 1:
-                    #heap = sorted(heap)[:-1]
-                    
-                    # heapq nlargest 활용
-                    val = heapq.nlargest(1, heap)[0]
-                    heap.remove(val)
-                elif int(data) == -1:
-                    heapq.heappop(heap)
-            else:
-                continue
     
-    return [max(heap),min(heap)] if heap else [0,0]
+    for op in operations:
+        if op.startswith('I'):
+            num = int(op.split()[-1])
+            heapq.heappush(heap, num)
+        
+        elif heap and (op == 'D -1'):
+            heapq.heappop(heap)
+        
+        elif heap and (op == "D 1"):
+            val = heapq.nlargest(1, heap)[0]
+            heap.remove(val)
+
+    return [max(heap), min(heap)] if heap else [0,0]
